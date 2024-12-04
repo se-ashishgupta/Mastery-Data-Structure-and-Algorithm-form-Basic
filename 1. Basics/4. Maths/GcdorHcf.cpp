ad430386c703292984497e022f5519d16c7fc02f
Ashish Gupta <gashish4950@gmail.com>
2024-12-04 09:04:04 +0530

    GCD or HCF done

---
+++ 1. Basics/4. Maths/GcdorHcf.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+int gcdNaive(int a, int b) // O(min(a,b))
+{
+    int m = min(a, b);
+
+    while (m > 0)
+    {
+        if (a % m == 0 && b % m == 0)
+        {
+            break;
+        }
+        m--;
+    }
+
+    return m;
+}
+
+// Euclied Algorithm
+// hcf(a,b) = hcf(a-b, b) a > b
+int gcdBetter(int a, int b) // O(min(a,b))
+{
+    while (a != b)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+
+    return a;
+}
+int gcdBetter1(int a, int b) // O(min(a,b))
+{
+    while (a > 0 && b > 0)
+    {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+
+    if (a == 0)
+        return b;
+
+    return a;
+}
+
+// Optimal
+int gcdOptimal(int a, int b) // O(log(min(a,b)))
+{
+
+    if (b == 0)
+        return a;
+    else
+        return gcdOptimal(b, a % b);
+}
+
+int main()
+{
+    int a = 20, b = 15;
+    cout << gcdOptimal(a, b);
+    return 0;
+}
