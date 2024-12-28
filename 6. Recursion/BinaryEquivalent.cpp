f0b1cea94f2ff9108cfc9bd3078d54c761aa07a7
Ashish Gupta <gashish4950@gmail.com>
2024-12-28 10:44:18 +0530

    done some basic question and binary equivalent

--- 6. Recursion/BinaryEquivalent.cpp
+++ 6. Recursion/BinaryEquivalent.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void pattern1(int n)
+void binaryEquivalent(int n)
 {
 
     if (n == 0)
         return;
-    pattern1(n / 2);
+    binaryEquivalent(n / 2);
     cout << (n % 2);
 }
 
 int main()
 {
 
-    pattern1(4);
+    binaryEquivalent(4);
     return 0;
 }
